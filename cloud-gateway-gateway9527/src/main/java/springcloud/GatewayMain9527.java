6c624d78af0949109afb8efbd41255445471ce99
wuliang0515 <1528141204.com>
2020-07-09 23:41:19 +0800

    gateway网关配置

---
+++ cloud-gateway-gateway9527/src/main/java/springcloud/GatewayMain9527.java
@@ -0,0 +1,17 @@
+package springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lynn
+ * @date 2020/5/16 - 9:40
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class, args);
+    }
+}
